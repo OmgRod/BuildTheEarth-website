78289ebf2279619e00bd4b995fc938662ad4340a
Nudesuppe42 <jufo.theo@gmail.com>
2024-11-11 21:56:49 +0100

    fix(dash/admin): :bug: Correct count when search query is present

--- apps/dashboard/src/app/am/users/page.tsx
+++ apps/dashboard/src/app/am/users/page.tsx
@@ -1,6 +1,5 @@
 import { Box, Title } from '@mantine/core';
 
-import { getSession } from '@/util/auth';
 import prisma from '@/util/db';
 import UsersDatatabe from './datatable';
 import { SearchUsers } from './interactivity';
@@ -10,22 +9,34 @@ export default async function Page({
 }: {
 	searchParams: Promise<{ page: string | undefined; query: string | undefined }>;
 }) {
-	const session = await getSession();
 	const page = (await searchParams).page;
 	const searchQuery = (await searchParams).query;
-	const userCount = await prisma.user.count();
-	console.log(searchQuery)
+	const userCount = await prisma.user.count({
+		where: searchQuery
+			? {
+					OR: [
+						{ username: { contains: searchQuery || undefined } },
+						{ minecraft: { contains: searchQuery || undefined } },
+						{ discordId: { contains: searchQuery || undefined } },
+						{ ssoId: { contains: searchQuery || undefined } },
+					],
+				}
+			: undefined,
+	});
+	console.log(searchQuery);
 	const users = await prisma.user.findMany({
 		take: 50,
 		skip: (Number(page || '1') - 1) * 50,
-		where: searchQuery ?{
-			OR: [
-				{ username: { contains: searchQuery || undefined } },
-				{ minecraft: { contains: searchQuery || undefined } },
-				{ discordId: { contains: searchQuery || undefined } },
-				{ ssoId: { contains: searchQuery || undefined } },
-			],
-		}:undefined,
+		where: searchQuery
+			? {
+					OR: [
+						{ username: { contains: searchQuery || undefined } },
+						{ minecraft: { contains: searchQuery || undefined } },
+						{ discordId: { contains: searchQuery || undefined } },
+						{ ssoId: { contains: searchQuery || undefined } },
+					],
+				}
+			: undefined,
 	});
 
 	return (
@@ -33,7 +44,7 @@ export default async function Page({
 			<Title order={1} mt="xl" mb="md">
 				Website Users
 			</Title>
-			<SearchUsers mb="md" maw="30%"/>
+			<SearchUsers mb="md" maw="30%" />
 			<UsersDatatabe users={users} count={userCount} />
 		</Box>
 	);
