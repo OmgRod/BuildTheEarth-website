6076bf7f6cb18ee8ff9aafebeb57f6dceca16530
Nudesuppe42 <jufo.theo@gmail.com>
2025-02-09 11:05:16 +0100

    fix(dash/src): :construction: Test env loading

--- apps/dashboard/src/app/me/everything/page.tsx
+++ apps/dashboard/src/app/me/everything/page.tsx
@@ -228,6 +228,8 @@ export default async function Page() {
 						language="json"
 						withCopyButton={false}
 					/>
+					<pre>{JSON.stringify(process.env, null, 2)}</pre>
+					<pre>{JSON.stringify(process.env.NEXT_PUBLIC_MAPBOX_TOKEN, null, 2)}</pre>
 				</TabsPanel>
 			</Tabs>
 		</Box>
