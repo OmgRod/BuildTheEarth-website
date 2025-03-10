f5232f36787296bf116491c8cd4b18febc2777a3
Nudesuppe42 <jufo.theo@gmail.com>
2025-03-10 21:50:22 +0100

    feat(sitemap): replace dynamic locales with static array in sitemap generation

--- apps/frontend/src/utils/Sitemap.ts
+++ apps/frontend/src/utils/Sitemap.ts
@@ -1,4 +1,3 @@
-import nextConfig from 'next.config';
 export function generateSiteMap(content: string) {
 	return `<?xml version="1.0" encoding="UTF-8"?>
             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
@@ -14,7 +13,7 @@ export function generateSiteMapContent(
 			(d) => `<url>
                         <loc>${`https://buildtheearth.net${d.loc}`}</loc>
                         <lastmod>${d.lastModified.toISOString().split('T')[0]}</lastmod>
-                        ${nextConfig.i18n?.locales
+                        ${['en', 'de', 'fr', 'nl', 'no', 'ru', 'zh']
 													.map(
 														(locale) =>
 															` <xhtml:link rel="alternate" hreflang="${locale}" href="https://buildtheearth.net/${locale}${d.loc}"/>`,
