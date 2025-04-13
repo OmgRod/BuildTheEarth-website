78cfeedb73d15da580e5d8dd9ed6ae901da3e6df
Nudesuppe42 <jufo.theo@gmail.com>
2025-04-13 17:36:52 +0200

    feat(credits): :sparkles: Enhance Credits page layout and update third-party licenses link

--- apps/frontend/src/pages/credits.tsx
+++ apps/frontend/src/pages/credits.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Container } from '@mantine/core';
+import { Anchor, TypographyStylesProvider } from '@mantine/core';
 
 import thumbnail from '@/public/images/thumbnails/contact.png';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -7,8 +7,8 @@ import Page from '../components/Page';
 
 const Credits = () => {
 	return (
-		<Page fullWidth head={{ title: 'Credits', image: thumbnail }}>
-			<Container w="80vw" style={{ listStyleType: 'none' }}>
+		<Page head={{ title: 'Credits', image: thumbnail }}>
+			<TypographyStylesProvider>
 				<h1>BuildTheEarth Website</h1>
 
 				<p>
@@ -60,106 +60,17 @@ const Credits = () => {
 					</li>
 				</ol>
 				<h2>Used Packages</h2>
-				<ol style={{ listStyleType: 'none' }}>
-					<li>@aws-sdk/client-s3</li>
-					<li>@bte-germany/terraconvert</li>
-					<li>@keycloak/keycloak-admin-client</li>
-					<li>@prisma/client</li>
-					<li>@turf/helpers</li>
-					<li>@turf/turf</li>
-					<li>blurhash</li>
-					<li>body-parser</li>
-					<li>cors</li>
-					<li>cron</li>
-					<li>dotenv</li>
-					<li>express</li>
-					<li>express-prom-bundle</li>
-					<li>express-session</li>
-					<li>express-validator</li>
-					<li>express-yup-middleware</li>
-					<li>jsonwebtoken</li>
-					<li>keycloak-connect</li>
-					<li>mariadb</li>
-					<li>minimatch</li>
-					<li>multer</li>
-					<li>mysql</li>
-					<li>prom-client</li>
-					<li>reflect-metadata</li>
-					<li>rfdc</li>
-					<li>sharp</li>
-					<li>uuid</li>
-					<li>winston</li>
-					<li>yup</li>
-					<li>@emotion/react</li>
-					<li>@emotion/server</li>
-					<li>@icons-pack/react-simple-icons</li>
-					<li>@mantine/carousel</li>
-					<li>@mantine/core</li>
-					<li>@mantine/dates</li>
-					<li>@mantine/dropzone</li>
-					<li>@mantine/form</li>
-					<li>@mantine/hooks</li>
-					<li>@mantine/modals</li>
-					<li>@mantine/next</li>
-					<li>@mantine/notifications</li>
-					<li>@mantine/nprogress</li>
-					<li>@mantine/prism</li>
-					<li>@mantine/rte</li>
-					<li>@mantine/spotlight</li>
-					<li>@mantine/tiptap</li>
-					<li>@mapbox/mapbox-gl-draw</li>
-					<li>@next/bundle-analyzer</li>
-					<li>@react-three/drei</li>
-					<li>@react-three/fiber</li>
-					<li>@tabler/icons-react</li>
-					<li>@tiptap/extension-highlight</li>
-					<li>@tiptap/extension-link</li>
-					<li>@tiptap/extension-subscript</li>
-					<li>@tiptap/extension-superscript</li>
-					<li>@tiptap/extension-text-align</li>
-					<li>@tiptap/extension-underline</li>
-					<li>@tiptap/pm</li>
-					<li>@tiptap/react</li>
-					<li>@tiptap/starter-kit</li>
-					<li>@types/mapbox__mapbox-gl-draw</li>
-					<li>@types/sanitize-html</li>
-					<li>axios</li>
-					<li>clsx</li>
-					<li>cookies-next</li>
-					<li>dayjs</li>
-					<li>embla-carousel-react</li>
-					<li>eslint-import-resolver-webpack</li>
-					<li>flag-icons</li>
-					<li>framer-motion</li>
-					<li>geolib</li>
-					<li>i18next</li>
-					<li>keycloak-j</li>
-					<li>mantine-contextmenu</li>
-					<li>mapbox-gl</li>
-					<li>mapbox-gl-style-switcher</li>
-					<li>next</li>
-					<li>next-auth</li>
-					<li>next-i18next</li>
-					<li>next-seo</li>
-					<li>oauth-pkce</li>
-					<li>react</li>
-					<li>react-dom</li>
-					<li>react-i18next</li>
-					<li>react-map-gl</li>
-					<li>sanitize-html</li>
-					<li>swr</li>
-					<li>tabler-icons-react</li>
-					<li>three</li>
-					<li>vague-time</li>
-				</ol>
 				<p>
 					A list of packages and their licenses can be found at{' '}
-					<Anchor component={Link} href={'https://buildtheearth.net/licenses.txt'}>
-						https://buildtheearth.net/licenses.txt
+					<Anchor
+						component={Link}
+						href={'https://raw.githubusercontent.com/BuildTheEarth/web/refs/heads/main/third-party-licenses.txt'}
+					>
+						third-party-licenses.txt
 					</Anchor>{' '}
-					and their respective github websites.
+					and their respective websites.
 				</p>
-			</Container>
+			</TypographyStylesProvider>
 		</Page>
 	);
 };
