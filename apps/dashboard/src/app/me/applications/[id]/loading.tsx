fe6da779348f5cbb2deba123504d702476f8fee3
Nudesuppe42 <jufo.theo@gmail.com>
2025-04-23 17:24:19 +0200

    feat(dash/me): :sparkles: Add loading state to application route

--- apps/dashboard/src/app/me/applications/[id]/loading.tsx
+++ apps/dashboard/src/app/me/applications/[id]/loading.tsx
@@ -2,18 +2,18 @@ import { Alert, Box, Flex, SimpleGrid, Skeleton, Text, Title } from '@mantine/co
 
 import { TextCard } from '@/components/core/card/TextCard';
 import { BuildTeamDisplay } from '@/components/data/BuildTeam';
-import { IconLoader } from '@tabler/icons-react';
+import { applicationStatusToAlert } from '@/util/transformers';
+import { ApplicationStatus } from '@repo/db';
 
 export default async function Page() {
 	return (
 		<Box mx="md" maw="90vw">
 			<Flex gap="sm" justify="flex-start" align="flex-end" direction="row" wrap="nowrap" mt="xl" mb="md">
-				<Title order={1}>Application XXXXXXXX</Title>
-				<Text c="dimmed" fz="sm">
-					( ... )
-				</Text>
+				<Title order={1}>
+					Application <Skeleton component={'span'}>xxxxxxxx</Skeleton>
+				</Title>
 			</Flex>
-			<SimpleGrid cols={2}>
+			<SimpleGrid cols={3}>
 				<Skeleton>
 					<TextCard isText={false} title="Build Region">
 						<BuildTeamDisplay team={{ icon: '', name: '', slug: '' }} noAnchor />
@@ -21,7 +21,12 @@ export default async function Page() {
 				</Skeleton>
 				<Skeleton>
 					<TextCard title="Created At" isText>
-						---
+						-/-
+					</TextCard>
+				</Skeleton>
+				<Skeleton>
+					<TextCard title="Reviewed At" isText>
+						-/-
 					</TextCard>
 				</Skeleton>
 			</SimpleGrid>
@@ -29,19 +34,25 @@ export default async function Page() {
 				<Alert
 					variant="light"
 					style={{
-						border: `calc(0.0625rem* var(--mantine-scale)) solid var(--mantine-color-orange-outline)`,
+						border: `calc(0.0625rem* var(--mantine-scale)) solid var(--mantine-color-green-outline)`,
 					}}
-					color="orange"
+					color={'green'}
 					radius="md"
-					title="Application loading"
-					icon={<IconLoader />}
+					title={'Loading...'}
 					h="100%"
 					my="md"
 				>
-					<Text>Loading your application... Please be patient.</Text>
+					<Text>{applicationStatusToAlert(ApplicationStatus.SEND).description}</Text>
 				</Alert>
 			</Skeleton>
-			<Skeleton h="60vh" mt="md"></Skeleton>
+			<Skeleton mt="md" mih="40vh">
+				<TextCard title="Application Answers">
+					<Text c="dimmed" size="md" mb="lg" maw="60%">
+						These are the answers you provided in your application. If you believe there is an error or you would like
+						to provide additional information, please contact the Build Region directly.
+					</Text>
+				</TextCard>
+			</Skeleton>
 		</Box>
 	);
 }
